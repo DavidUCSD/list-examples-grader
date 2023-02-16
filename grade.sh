6cb6ef60d855fcc29b4b5bf8b3b29937bf34b1f0
DavidUCSD <floresdavid217@gmail.com>
2023-02-16 13:38:19 -0800

    d

--- grade.sh
+++ grade.sh
@@ -3,13 +3,13 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
-if [ -f student-submission/.java ]; then
+if [[ -f student-submission/ListExamples.java ]]; then
   echo "Student Submission Successful"
   mkdir testFolder
   cp student-submission/.txt testFolder
   cp TestListExamples.java testFolder
   javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-  if [ $? -eq 0 ]; then
+  if [[] $? -eq 0 ]]; then
     set +e
     java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > testResults.txt
     grep -e "# of Tests: " -e "# of Failures" -e "# of Errors" testResults.txt
