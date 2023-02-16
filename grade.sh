17c24f34d00f2836f976bad07c9410cc01f8bf39
DavidUCSD <floresdavid217@gmail.com>
2023-02-16 13:20:42 -0800

    Update grade.sh
    Uodated GRade.sh

--- grade.sh
+++ grade.sh
@@ -1,5 +1,22 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
-git clone $1 student-submission
-echo 'Finished cloning'
+    git clone $1 student-submission
+    echo 'Finished cloning'
+if[-f student-student/*.java];
+    echo "Student Submission Successful"
+    mkdir testFolder
+    cp student-submission/*.txt > testFolder
+    cp TestListExamples.java testFolder
+    javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 
+if[ $? -eq 0 ] then
+    set +e
+    java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > testResults.txt 
+    grep -e "# of Tests: " -e "# of Failures: " -e "# of Errors" testResults.txt
+else
+    echo "Tests Failed to Compile"
+    exit 
+else 
+    echo "Incorrect File, please submit the correct file."
+    exit 
+fi
