07003b662ab82bacf609c8c2f31cce226d5d3989
DavidUCSD <floresdavid217@gmail.com>
2023-02-16 13:44:44 -0800

    updfsa

--- grade.sh
+++ grade.sh
@@ -5,9 +5,7 @@ git clone $1 student-submission
 echo 'Finished cloning'
 if [[ -f student-submission/ListExamples.java ]]; then
   echo "Student Submission Successful"
-  mkdir testFolder
-  cp student-submission/.txt testFolder
-  cp TestListExamples.java testFolder
+  cp ListExamples.java ./ 
   javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
   if [[ $? -eq 0 ]]; then
     set +e
