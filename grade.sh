a8fe2186c4ebe80f52df47980975b60060e1403a
DavidUCSD <floresdavid217@gmail.com>
2023-02-16 13:39:24 -0800

    d

--- grade.sh
+++ grade.sh
@@ -9,7 +9,7 @@ if [[ -f student-submission/ListExamples.java ]]; then
   cp student-submission/.txt testFolder
   cp TestListExamples.java testFolder
   javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-  if [[] $? -eq 0 ]]; then
+  if [[ $? -eq 0 ]]; then
     set +e
     java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > testResults.txt
     grep -e "# of Tests: " -e "# of Failures" -e "# of Errors" testResults.txt
