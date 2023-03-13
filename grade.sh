577f9495ce00f4cd88545ce4842da8ddb365edd4
DavidUCSD <floresdavid217@gmail.com>
2023-03-13 11:33:13 -0700

    m

--- grade.sh
+++ grade.sh
@@ -3,14 +3,20 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
 if [[ -f student-submission/ListExamples.java ]]; then
-  echo "Student Submission Successful"
+  echo "Student Submission Successful" 
   cp student-submission/ListExamples.java ./ 
   javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-  if [[ $? -eq 0 ]]; then
+
+if [[ $? -eq 0 ]]; then
     java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > testResults.txt
-    grep -e "# of Tests: " -e "# of Failures" -e "# of Errors" testResults.txt
-  else
+    passedTest=$(grep -c 'OK' testResults.txt)
+    failedTest=$(grep -c 'FAILURES!!!' testResults.txt)
+    totalTest=passedTest+failedTest
+    score=$((100*(passedTest/totalTest)))
+    echo "Final Grade: $score%"
+ else 
     echo "Tests failed to compile"
     exit
   fi
